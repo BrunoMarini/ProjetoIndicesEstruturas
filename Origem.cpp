4670fc585c704688e69cc1d90f99a8b7013c855a
BrunoMarini <bgmarini@hotmail.com>
2018-10-09 09:27:25 -0300

    CriaArquivoBinario

--- Origem.cpp
+++ Origem.cpp
@@ -1,59 +1,36 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 
-enum menu {	consultarRegistros = 1, alterarRegistro, apagarRegistro, inserirRegistro, impressao};
+typedef struct {
+	int chave;
+	char nomeCarro[50];
+	char montadora[50];
+	float consumo;
+	int peso;
 
-void adicionarRegistros(FILE*);
+} no;
 
-void main()
+void main() 
 {
-	menu opcao;
-	FILE* arquivo;
+	FILE *p;
+	no aux;
+	int tam = 50;
 
+	p = fopen("database.dat", "wb");
 
+	fwrite(&tam, sizeof(int), 1, p);
 
-
-	arquivo = fopen("carros.dat", "a+b");
-
-	printf("-------------------- Menu --------------------\n");
-	printf(" 1. Consultar um registro;\n");
-	printf(" 2. ALterar/Atualizar campo(s) de um registro;\n");
-	printf(" 3. Eliminar um registro do arquivo;\n");
-	printf(" 4. Inserir um registro no arquivo;\n");
-	printf(" 5. Impressao;\n\n");
-	printf(" Opcao desejada: ");
-	scanf("%d", &opcao);
-
-	switch (opcao)
+	for (int i = 0; i < tam; i++)
 	{
-	case consultarRegistros:
-		break;
-	case alterarRegistro:
-		break;
-	case apagarRegistro:
-		break;
-	case inserirRegistro:
-		adicionarRegistros(arquivo);
-		break;
-	case impressao:
-		break;
-	default:
-		break;
+		scanf("%i", &aux.chave);
+		fgets(aux.nomeCarro, 50, stdin);
+		fgets(aux.montadora, 50, stdin);
+		scanf("%f", &aux.consumo);
+		scanf("%i", &aux.peso);
+
+		fwrite(&aux, sizeof(no), 1, p);
 	}
 
-
-
 	system("pause");
-}
-
-void adicionarRegistros(FILE* arq)
-{
-//	int n;
-
-	arq = fopen("carros.dat", "a+b");
-
-	
-
 }
