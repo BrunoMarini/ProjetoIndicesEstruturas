0e05738ce0186cba7d64299eae49405ab1d00823
BrunoMarini <bgmarini@hotmail.com>
2018-10-13 13:58:14 -0300

    Impressao Categoria Nome

--- Principal.cpp
+++ Principal.cpp
@@ -33,7 +33,8 @@ no* carregarBaseDados(int*, no*);
 no* modificarRegistro(no*, listaDenso*, int);
 no* eliminarRegistro(no*, listaDenso*, int*);
 listaDenso* criarIndiceDenso(int, no*, listaDenso*);
-void impressoes(no*, listaDenso*, int, int, int*);
+void impressoes(no*, listaDenso*, int, int, int*, char[5][50], int[], int[]);
+void catChaveOrd(no*, int[], int*, int, char[5][50]);
 void consultarRegistro(no*, listaDenso*, int);
 void bubble(listaDenso[], int);
 int buscaBinaria(listaDenso*, int, int);
@@ -48,10 +49,14 @@ void main()
 	listaDenso *indiceDenso = NULL;
 	int qtdCadastros, count, posicao;
 	int *ordemAlfabetica = NULL, pri;
+	int pri_cat[5], catNome[100];
+	char montadoras[5][50] = { "Volkswagen", "Chevrolet", "Ford", "Fiat", "Hyundai" };
 
 	cadastros = carregarBaseDados(&qtdCadastros, cadastros);
 	indiceDenso = criarIndiceDenso(qtdCadastros, cadastros, indiceDenso);
 	ordemAlfabetica = tabelaInversa(ordemAlfabetica, &pri, cadastros, qtdCadastros);
+	catChaveOrd(cadastros, pri_cat, catNome, qtdCadastros, montadoras);
+
 
 	do
 	{
@@ -74,19 +79,22 @@ void main()
 		case alterarRegistro:
 			cadastros = modificarRegistro(cadastros, indiceDenso, qtdCadastros);
 			ordemAlfabetica = tabelaInversa(ordemAlfabetica, &pri, cadastros, qtdCadastros);
+			catChaveOrd(cadastros, pri_cat, catNome, qtdCadastros, montadoras);
 			break;
 		case apagarRegistro:
 			cadastros = eliminarRegistro(cadastros, indiceDenso, &qtdCadastros);
 			indiceDenso = criarIndiceDenso(qtdCadastros, cadastros, indiceDenso);
 			ordemAlfabetica = tabelaInversa(ordemAlfabetica, &pri, cadastros, qtdCadastros);
+			catChaveOrd(cadastros, pri_cat, catNome, qtdCadastros, montadoras);
 			break;
 		case inserirRegistro:
 			cadastros = adicionarRegistros(cadastros, &qtdCadastros);
 			indiceDenso = criarIndiceDenso(qtdCadastros, cadastros, indiceDenso);
 			ordemAlfabetica = tabelaInversa(ordemAlfabetica, &pri, cadastros, qtdCadastros);
+			catChaveOrd(cadastros, pri_cat, catNome, qtdCadastros, montadoras);
 			break;
 		case impressao:
-			impressoes(cadastros, indiceDenso, qtdCadastros, pri, ordemAlfabetica);
+			impressoes(cadastros, indiceDenso, qtdCadastros, pri, ordemAlfabetica, montadoras, pri_cat, catNome);
 			break;
 		case sair:
 			break;
@@ -302,15 +310,15 @@ no* modificarRegistro(no* cadastros, listaDenso* indice, int qtd)
 
 }
 
-void impressoes(no* cadastros, listaDenso* lista, int qtd, int pri, int *tabelaInversaAlfabeto)
+void impressoes(no* cadastros, listaDenso* lista, int qtd, int pri, int *tabelaInversaAlfabeto, char montadoras[5][50], int pri_cat[], int catNome[])
 {
-	int i, j;
+	int i, j, po;
 	int posicao;
 	menuImpressao opcao;
 
 	system("cls");
 
-	printf("----- Menu Impressao -----\n\n");
+	printf("\n----- Menu Impressao -----\n\n");
 	printf("1. Imprimir todos por chave primaria ordenada;\n");
 	printf("2. Imprimir todos ordenado pelo nome do carro;\n");
 	printf("3. Imprimir todos por categoria ordenados pela chave;\n");
@@ -351,8 +359,6 @@ void impressoes(no* cadastros, listaDenso* lista, int qtd, int pri, int *tabelaI
 
 		while (i > 0)
 		{
-				
-
 				printf(" %i   ", cadastros[posicao].chave);
 				printf("%s ", cadastros[posicao].nomeCarro);
 				for (j = 15 - strlen(cadastros[posicao].nomeCarro); j > 0; j--) printf(" ");
@@ -365,10 +371,33 @@ void impressoes(no* cadastros, listaDenso* lista, int qtd, int pri, int *tabelaI
 				posicao = tabelaInversaAlfabeto[posicao];
 				i--;
 		}
+
 		break;
 	case categoriaChaveOrdenado:
 		break;
 	case categoriaNomeOrdenado:
+
+		for (i = 0; i < 5; i++)
+		{
+			printf("\n\n --------------  %s  --------------\n\n", montadoras[i]);
+			   printf(" Chave  Nome Carro      Consumo  Peso\n\n");
+
+			posicao = pri_cat[i];
+
+			do
+			{
+				printf(" %i   ", cadastros[posicao].chave);
+				printf("%s ", cadastros[posicao].nomeCarro);
+				for (j = 15 - strlen(cadastros[posicao].nomeCarro); j > 0; j--) printf(" ");
+				if (cadastros[posicao].consumo < 10) printf("0");
+				printf("%.2f ", cadastros[posicao].consumo);
+				printf("   %i\n", cadastros[posicao].peso);
+				posicao = catNome[posicao];
+
+			} while (posicao != -1);
+
+		}
+
 		break;
 	case imprimirArquivo:
 
@@ -598,4 +627,114 @@ int* tabelaInversa(int *ordem, int *pri, no* cadastros, int qtd)
 	}
 
 	return (ordem);
+}
+
+void catChaveOrd(no* cadastros, int pri_cat[], int *cadas, int qtd, char montadoras[5][50])
+{
+	int count[5] = { 0 };
+	int i, j;
+	int countMont[5] = { 0 };
+
+	for (i = 0; i < 100; i++)
+		cadas[i] = -1;
+
+
+	tabInv *mont1, *mont2, *mont3, *mont4, *mont5;
+
+	for (i = 0; i < qtd; i++)
+	{
+		     if (strcmp(cadastros[i].montadora, montadoras[0]) == 0)
+			count[0]++;
+		else if (strcmp(cadastros[i].montadora, montadoras[1]) == 0)
+			count[1]++;
+		else if (strcmp(cadastros[i].montadora, montadoras[2]) == 0)
+			count[2]++;
+		else if (strcmp(cadastros[i].montadora, montadoras[3]) == 0)
+			count[3]++;
+		else if (strcmp(cadastros[i].montadora, montadoras[4]) == 0)
+			count[4]++;
+	}
+
+	mont1 = (tabInv*)malloc(count[0] * sizeof(tabInv));
+	mont2 = (tabInv*)malloc(count[1] * sizeof(tabInv));
+	mont3 = (tabInv*)malloc(count[2] * sizeof(tabInv));
+	mont4 = (tabInv*)malloc(count[3] * sizeof(tabInv));
+	mont5 = (tabInv*)malloc(count[4] * sizeof(tabInv));
+
+	for (i = 0; i < qtd; i++)
+	{
+		if (strcmp(cadastros[i].montadora, montadoras[0]) == 0) //VOLKSWAGEN
+		{
+			strcpy(mont1[countMont[0]].nome, cadastros[i].nomeCarro);
+			mont1[countMont[0]].posicao = i;
+			countMont[0]++;
+		}
+		else if (strcmp(cadastros[i].montadora, montadoras[1]) == 0) //CHEVROLET
+		{
+			strcpy(mont2[countMont[1]].nome, cadastros[i].nomeCarro);
+			mont2[countMont[1]].posicao = i;
+			countMont[1]++;
+		}
+		else if (strcmp(cadastros[i].montadora, montadoras[2]) == 0) //FORD
+		{
+			strcpy(mont3[countMont[2]].nome, cadastros[i].nomeCarro);
+			mont3[countMont[2]].posicao = i;
+			countMont[2]++;
+		}
+		else if (strcmp(cadastros[i].montadora, montadoras[3]) == 0) //FIAT
+		{
+			strcpy(mont4[countMont[3]].nome, cadastros[i].nomeCarro);
+			mont4[countMont[3]].posicao = i;
+			countMont[3]++;
+		}
+		else if (strcmp(cadastros[i].montadora, montadoras[4]) == 0) //HYUNDAI
+		{
+			strcpy(mont5[countMont[4]].nome, cadastros[i].nomeCarro);
+			mont5[countMont[4]].posicao = i;
+			countMont[4]++;
+		}
+	}
+
+	bubbleString(mont1, countMont[0]);
+	bubbleString(mont2, countMont[1]);
+	bubbleString(mont3, countMont[2]);
+	bubbleString(mont4, countMont[3]);
+	bubbleString(mont5, countMont[4]);
+
+	pri_cat[0] = mont1[0].posicao;
+	pri_cat[1] = mont2[0].posicao;
+	pri_cat[2] = mont3[0].posicao;
+	pri_cat[3] = mont4[0].posicao;
+	pri_cat[4] = mont5[0].posicao;
+
+	for(i = 0; i < countMont[0]; i++)
+		cadas[mont1[i].posicao] = mont1[i + 1].posicao;
+	
+	cadas[mont1[i - 1].posicao] = (-1);
+
+	for (i = 0; i < countMont[1]; i++)
+		cadas[mont2[i].posicao] = mont2[i + 1].posicao;
+	
+	cadas[mont2[i - 1].posicao] = (-1);
+
+	for (i = 0; i < countMont[2]; i++)
+		cadas[mont3[i].posicao] = mont3[i + 1].posicao;
+	
+	cadas[mont3[i - 1].posicao] = (-1);
+
+	for (i = 0; i < countMont[3]; i++)
+		cadas[mont4[i].posicao] = mont4[i + 1].posicao;
+	
+	cadas[mont4[i - 1].posicao] = (-1);
+
+	for (i = 0; i < countMont[4]; i++)
+		cadas[mont5[i].posicao] = mont5[i + 1].posicao;
+
+	cadas[mont5[i - 1].posicao] = (-1);
+
+	free(mont1);
+	free(mont2);
+	free(mont3);
+	free(mont4);
+	free(mont5);
 }
